d33b20422ab4865cf9336c222b4d90a0670d96ae
sayidimrannazari <Sayidi_n@yahoo.com>
2022-01-29 16:58:42 -0500

    api third

--- src/test/java/com/cybertek/tests/day04_ORDS_Path/GetSpartansUsingPath.java
+++ src/test/java/com/cybertek/tests/day04_ORDS_Path/GetSpartansUsingPath.java
@@ -5,6 +5,9 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,16 +49,59 @@ public class GetSpartansUsingPath extends SpartanTestBase {
         int id1 = response.path("id");
         String name = response.path("name");
         String gender = response.path("gender");
+        long phoneNum = response.path("phone");
 
         //validate the above
         assertEquals(13,id1);
         assertEquals("Jaimie",name);
         assertEquals("Female",gender);
-        assertEquals("7842554879",response.path("phone").toString());
+        assertEquals(7842554879L,phoneNum);
 
 
 
 
     }
+
+    /**
+     Given accept is json
+     When I send get request to /api/spartans
+     Then status code is 200
+     And content type is json
+     And I can navigate json array object
+     */
+
+    @Test
+    public void readJsonArrayTest() {
+
+        Response response = given().accept(ContentType.JSON).and().get("api/spartans");
+
+        System.out.println(response.statusCode());
+
+        Assertions.assertEquals(200,response.statusCode());
+
+        System.out.println("Content type = " + response.contentType());
+        assertEquals("application/json",response.contentType());
+
+        System.out.println("id = "+ response.path("id"));
+        System.out.println("id of first spartan = " + response.path("[0].id"));
+        System.out.println("name of the second person = " + response.path("[1].name"));
+
+        System.out.println("First 10 names");
+        for(int i=0;i<10;i++){
+            System.out.println(response.path("["+i+"].id")+" = "+response.path("["+i+"].name"));
+        }
+
+        //Store all first names into a list
+        List<String > allNames = response.path("names");
+        System.out.println("names count = "+ allNames.size());
+
+        for (String eachName : allNames){
+            System.out.println(eachName);
+        }
+
+
+
+    }
+
 
 }
