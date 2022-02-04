1897dba42be60b27bdc4aedfe1accab1bff2b7c6
sayidimrannazari <Sayidi_n@yahoo.com>
2022-02-03 21:47:33 -0500

    pojo

--- src/test/java/com/cybertek/tests/day07_deserialization/AllSpartansToPojoTest.java
+++ src/test/java/com/cybertek/tests/day07_deserialization/AllSpartansToPojoTest.java
@@ -3,11 +3,14 @@ package com.cybertek.tests.day07_deserialization;
 import com.cybertek.tests.SpartanTestBase;
 import com.cybertek.tests.pojo.AllSpartans;
 import com.cybertek.tests.pojo.Spartan;
+import com.cybertek.tests.pojo.SpartanSearch;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static io.restassured.RestAssured.*;
@@ -34,5 +37,43 @@ public class AllSpartansToPojoTest extends SpartanTestBase {
                 System.out.println(each.toString());
             }
         }
+    }
+
+
+    @Test
+    public void searchSpartansToPojoTest() {
+
+        Map<String,String> spartanMap = new HashMap<>();
+        spartanMap.put("nameContains","ai");
+
+        Map<String,String> spartanMap1 = new HashMap<>();
+        spartanMap1.put("gender","female");
+
+        Response response = given().accept(ContentType.JSON)
+                           .queryParams(spartanMap)
+                           .and().queryParams(spartanMap1)
+                             .when().get("api/spartans/search");
+
+        System.out.println(response.statusCode());
+
+        SpartanSearch spartanSearch = response.as(SpartanSearch.class);
+        System.out.println("total elements = " + spartanSearch.getTotalElement());
+        assertEquals(3,spartanSearch.getTotalElement());
+        System.out.println("Spartans info content = " + spartanSearch.getContent());
+
+        //Store jaimie info into separate variable
+
+        Spartan first = spartanSearch.getContent().get(1);
+        System.out.println(first);
+        System.out.println("id = " + first.getId());
+        System.out.println("name = " + first.getName());
+        System.out.println("gender = " + first.getGender());
+        System.out.println("phone = " + first.getPhone());
+
+        assertEquals(13,first.getId());
+        assertEquals("Jaimie",first.getName());
+        assertEquals("Female",first.getGender());
+        assertEquals(7842554879L,first.getPhone());
+
     }
 }
---
+++ src/test/java/com/cybertek/tests/pojo/Region.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Region {
+
+    private int region_id;
+    private String region_name;
+}
